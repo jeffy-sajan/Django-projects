64827df62c14afec0bfd3ead496df7b9d6036b0c
jeffy-sajan <149129289+jeffy-sajan@users.noreply.github.com>
2023-12-29 21:23:13 +0530

    Delete moviepro directory

--- moviepro/movie/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from movie.models import Movie
-
-admin.site.register(Movie)
--- moviepro/movie/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from movie.models import Movie
-
-
-class movieform(forms.ModelForm):
-    class Meta:
-        model=Movie
-        fields='__all__'
